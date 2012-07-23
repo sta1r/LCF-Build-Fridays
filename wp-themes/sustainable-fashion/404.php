8d7e17d13c98d4460da397ab4c6b28cfe271e713
Alastair Mucklow <a.mucklow@fashion.arts.ac.uk>
2012-07-23 12:36:41 +0100

    Remove themes folder - these will be tracked via a separate repo

--- wp-themes/sustainable-fashion/404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-	<div id="main" class="grid_22 prefix_2">
-		<div id="content">			
-			<article>
-				<h2>Error 404 - Not Found</h2>
-			</article>	
-		</div>
-	</div>
-<?php get_footer(); ?>
--- wp-themes/sustainable-fashion/metaboxes/custom-spec.php
+++
@@ -1,8 +0,0 @@
-<?php
-$custom_metabox = new WPAlchemy_MetaBox(array
-(
-	'id' => '_custom_meta',
-	'title' => 'Extra fields',
-	'types' => array('page'),
-	'template' => get_stylesheet_directory() . '/metaboxes/custom-meta.php',
-));
