ddb8c49b0361316416e2b972e520e995580b7466
cgush <c.gush@fashion.arts.ac.uk>
2012-11-19 17:29:31 +0000

    Added Grad Info evening
    No select button - rsvp directly to Jason

--- openday-booking/index.php
+++ openday-booking/index.php
@@ -11,159 +11,7 @@ include('header.php');
 			<p>Please choose your area of interest from the list below and click 'Select' to book your place. Our postgraduate dates will be advertised shortly.</p>
 
 		 <ul class="list-folding">
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Fashion Design</span></a><br /><span class="bodycopy"><strike>Saturday 15 September and Saturday 6 October, 11am (John Prince’s Street)</strike> *FULLY BOOKED*</span></div>
 
-				<div id="list1" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>BA (Hons) Fashion Design Technology: Menswear and Womenswear (11am both dates)</li>
-						<li>FdA Designer Pattern Cutter (11am both dates)</li>
-						<li>Becoming an LCF Student (12pm both dates)</li>
-						<li>BA (Hons) Fashion Design and Development, Contour and Sportswear (1pm both dates)</li>
-						<li>MDes International Fashion Production Management (1pm both dates)</li>
-					</ul>
-					<p>Tours will be available at our Curtain Road site after the presentation.</p>
-
-					<form action="booking.php" method="get">    
-					<input type="hidden" value="fd" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form>
-				</div>
-			</li> --> <!-- #list1 --> 
-
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Textiles and Fashion Design and Marketing</span></a><br /><span class="bodycopy"><strike>Wednesday 19 September (Lime Grove) and Wednesday 10 October, 11am (John Prince's St)</strike></span> *FULLY BOOKED*</div>
-
-				<div id="list2" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>FdA Fashion Design and Marketing (11am both dates)</li>
-						<li>Becoming an LCF Student (12pm both dates)</li>
-						<li>FdA Fashion Design Technology (Knitwear and Surface Textiles) (1pm both dates)</li>
-						<li>BA (Hons) Fashion Design Technology: Fashion Textiles (1pm both dates)</li>			
-					</ul>
-
-				 	<form action="booking.php" method="get">   
-					<input type="hidden" value="tm" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form>
--->
-<!--					
-					<p><strong>Please note the 10 October open day will now take place at our John Prince's Street site.</strong></p>
-				</div>
-				
-
-			</li>-->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Cordwainers Footwear/Accessories and Fashion Jewellery</span></a><br /><span class="bodycopy"><strike>Wednesday 17 October</strike> *FULLY BOOKED*</span></div>
->>>>>>> Charlotte
-
-				<div id="list3" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>FdA Cordwainers Footwear (11am)</li>
-						<li>BA(Hons) Cordwainers Footwear (11am)</li>
-						<li>Becoming an LCF Student (12pm)</li>
-						<li>BA (Hons) Cordwainers Accessories (1pm)</li>
-						<li>BA (Hons) Fashion Jewellery (1pm)</li>
-					</ul>
--->
-<!--					<form action="booking.php" method="get">    
-					<input type="hidden" value="cf" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form> -->
-<!--				</div>
-			</li> --> <!-- #list3 -->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Tailoring</span></a><br /><span class="bodycopy"><strike>Wednesday 31 October, 1pm (Mare Street)</strike> *FULLY BOOKED*</span></div>
-
-				<div id="list4" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>Becoming an LCF Student (1pm)</li>
-						<li>FdA Tailoring</li>
-						<li>BA (Hons) Bespoke Tailoring (2pm)</li>
-					</ul> -->
-
-			<!--		<form action="booking.php" method="get">    
-					<input type="hidden" value="ta" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form> -->
-<!--				</div>
-			</li> --><!-- #list4 -->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Fashion Management and Fashion Business</span></a><br /><span class="bodycopy"><strike>Wednesday 26 September and Saturday 13 October, 11am (High Holborn)</strike></span> *FULLY BOOKED* </div>
-
-				<div id="list5" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>Introduction from the Dean of Management and Science (11am both dates)</li>
-						<li>BA (Hons) Fashion Management (12pm both dates)</li>
-						<li>MSc International Fashion Management (12pm both dates)</li>
-						<li>FdA Fashion Buying and Merchandising (12pm and 1.30pm both dates)</li>
-						<li>FdA Fashion Marketing and Promotion (12pm and 1.30pm both dates)</li>
-						<li>FdA Fashion Retail Branding and Visual Merchandising (3.30pm both dates)</li>
-					</ul>
-					<p>Please note FdA Fashion Retail Branding and Visual Merchandising sessions will be held at John Prince’s Street site after the Introductory session at High Holborn. Students will be shown how to get there on the day</p>
-
-					<form action="booking.php" method="get">    
-					<input type="hidden" value="mb" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form>
-				</div>
-			</li> --> <!-- #list5 -->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Cosmetic Science and Beauty and Spa Management</span></a><br /><span class="bodycopy">Friday 12 October, 11am (John Prince’s Street)</span></div>
-
-				<div id="list6" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>MSc Cosmetic Science (Integrated MSc with BSc exit point) (11am)</li>
-						<li>Becoming an LCF Student (12pm)</li>
-						<li>FdSc Beauty and Spa Management (1pm)</li>
-					</ul>
-					<p>A taster session is available for students interested in MSc Cosmetic Science between 2pm-5pm. To book a place please email opendays@fashion.arts.ac.uk as spaces are limited.</p>
-
-					<form action="booking.php" method="get">    
-					<input type="hidden" value="cs" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form>
-				</div>
-			</li> --> <!-- #list6 -->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Performance Design</span></a><br /><span class="bodycopy"><strike>Saturday 20 October, 11am (Lime Grove)</strike> *FULLY BOOKED*</span></div>
-
-				<div id="list7" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>BA (Hons) Technical Effects for Performance (11am both dates)</li>
-						<li>BA (Hons) Make Up and Prosthetics for Performance (12pm both dates)</li>
-						<li>FdA Hair and Make Up for Film and TV (1pm both dates)</li>
-						<li>BA (Hons) Costume for Performance (2pm both dates)</li>
-						<li>Becoming an LCF Student (3pm both dates)</li>
-					</ul>
--->
-<!--					<form action="booking.php" method="get">    
-					<input type="hidden" value="pd" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form> -->
-<!--				</div>
-			</li> --> <!-- #list7 -->
-<!--			<li>
-				<div class="title expander"><a href="#"><span>Fashion Media and Communications</span></a><br /><span class="bodycopy"><strike>Saturday 27 October, 10.30am (Lime Grove)</strike> *FULLY BOOKED*</span></div>
-
-				<div id="list8" class="sublist_content bodycopy">
-					<ul class="inner">
-						<li>BA (Hons) Fashion Public Relations (10.30am)</li>
-						<li>BA (Hons) Fashion Journalism (10.30am)</li>
-						<li>Becoming an LCF Student (11.30am)</li>
-						<li>FdA Hair and Make Up for Fashion (12pm)</li>
-						<li>BA (Hons) Fashion Photography (1pm)</li>
-						<li>FdA Fashion Styling and Photography (2pm)</li>
-						<li>BA (Hons) Creative Direction for Fashion (3pm)</li>
-						<li>BA (Hons) Fashion Illustration (4pm)</li>
-					</ul>
--->
-				<!--	<form action="booking.php" method="get">    
-					<input type="hidden" value="mc" name="openday">	
-					<p><input type="submit" value="Select" /></p>
-					</form>  -->
-<!--				</div>
-			</li> --><!-- #list8 -->
 			<li>
 				<div class="title expander"><a href="#"><span>Part Time Degrees</span></a><br /><span class="bodycopy">Wednesday 28 November (6pm) (High Holborn), Wednesday 27 February (6pm) and Saturday 11 May (10.30am) (John Prince’s Street)</span></div>
 
@@ -198,6 +46,24 @@ include('header.php');
 					</form>
 				</div>
 			</li><!-- #list10 -->
+			
+			<li>
+				<div class="title expander"><a href="#"><span>Graduate School Information Evening</span></a><br /><span class="bodycopy">Monday 3rd December (6pm - 7:30pm), John Prince's Street</span></div>
+
+				<div id="list11" class="sublist_content bodycopy">
+					<p>An exclusive opportunity to find out everything you need to know about applying to a graduate school course at London College of Fashion.</p>
+					<ul class="inner">
+						<li>MA Fashion degrees</li>
+						<li>Executive MBA Fashion</li>
+						<li>Postgraduate Diplomas</li>
+						<li>Postgraduate Certificates</li>
+						<li>Graduate Diplomas</li>
+					</ul>
+					<p>During this evening Course Advisors will be on hand to provide information on how to apply, entry requirements, scholarships and future postgraduate events. You will also be able to enjoy a drink with us and attend our latest exhibition Leather: Second Skin showcasing work by ex MA students Oliver Ruuger, Rob Goodwin and Volker Koch.</p>
+					<p>If you would like to attend this evening please RSVP to <a href="mailto:j.clapperton@fashion.arts.ac.uk">j.clapperton@fashion.arts.ac.uk</a> with your name and course of interest.</p>					
+				</div>
+			</li><!-- #list11 -->
+				
 
 		</ul><!-- .list-folding -->
 		
