041634176c486f9abd82396d2c42eaa634892ff9
cgush <c.gush@fashion.arts.ac.uk>
2013-03-22 14:16:44 +0000

    Accommodation open day updates

--- openday-booking/index.php
+++ openday-booking/index.php
@@ -12,15 +12,17 @@ include('header.php');
 			<p>The undergraduate open days for all BA (Hons) subjects will take place in September and October 2013 – further information will be available in due course.</p>
 
 		 <ul class="list-folding">
-			
 			<li>
-				<div class="title expander"><a href="#"><span>Short Courses Open Afternoon</span></a><br /><span class="bodycopy"><strike>Saturday 9th March (12pm-4.30pm) (Lime Grove)</strike> *FULLY BOOKED*</span></div>
+				<div class="title expander"><a href="#"><span>Halls of Residence open days</span></a><br /><span class="bodycopy">Wednesday 27 March (12 - 6pm)</span></div>
 
-				<div id="list15" class="sublist_content bodycopy">
-					<p>Please note that this open afternoon is now fully booked.</p>			
-				</div> <!-- #list15-->
-			</li>
+				<div id="list8" class="sublist_content bodycopy">
+						<p>Most of our halls of residence will be open from 12-6pm for visitors to come and explore before booking opens in May. Students and current residents will be on hand to show you around each building.</p>
+						<p>No booking is required at any halls, however Coopers Court is not open as it will not be available next year and Glassyard Building will not be open to visitors until April.</p>
+						<p>For addresses and transport details of all our halls, please visit their individual information pages at <a href='http://www.arts.ac.uk/housing/ '>www.arts.ac.uk/housing/</a></p>
+						<p>We look forward to seeing you there.</p>
 
+				</div>
+			</li><!-- #list8 -->
 			<li>
 				<div class="title expander"><a href="#"><span>Part Time Degrees</span></a><br /><span class="bodycopy">Saturday 11 May (10.30am) (John Prince’s Street)</span></div>
 
