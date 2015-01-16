91735ba068a0ce669ca2d876e2d2f49f74ab7b6d
Alastair Mucklow <amucklow@strangerpixel.com>
2015-01-16 11:33:46 +0000

    Remove legacy / confidential files

--- openday-booking/mail_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$headers = 'From: London College of Fashion <opendays@fashion.arts.ac.uk>' . "\r\n" .
-  'Reply-To: opendays@fashion.arts.ac.uk' . "\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-
-mail('amucklow@strangerpixel.com', 'Test Mail', 'Test Message', $headers);
-
-?>
